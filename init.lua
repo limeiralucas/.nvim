f4849a39e11034dda5a065d6a48bf0bf635025e8
Lucas Limeira <lucasalveslm@gmail.com>
2023-03-17 23:14:53 -0300

    fix linenumbers in fern

--- init.lua
+++ init.lua
@@ -58,6 +58,11 @@ lazy.setup({
   {'lewis6991/gitsigns.nvim'},
   {'chentoast/marks.nvim'},
   {'akinsho/bufferline.nvim'},
+  {
+    'lambdalisue/fern.vim',
+    cmd = 'Fern',
+    dependencies = {'TheLeoP/fern-renderer-web-devicons.nvim'}
+  },
 })
 
 -- Colorscheme
@@ -72,7 +77,7 @@ require('lualine').setup({
     component_separators = '|',
     section_separators = '',
     disabled_filetypes = {
-      statusline = {'NvimTree'}
+      statusline = {'fern'}
     }
   }
 })
@@ -113,7 +118,7 @@ require('bufferline').setup({
   options = {
     mode = 'buffers',
     offsets = {
-      {filetype = 'NvimTree'}
+      {filetype = 'fern'}
     },
   },
   highlights = {
@@ -127,6 +132,17 @@ require('bufferline').setup({
   }
 })
 
+-- Fern
+vim.keymap.set('n', '<leader>e', '<cmd>Fern . -drawer -toggle -right<cr>')
+vim.g['fern#renderer'] = 'nvim-web-devicons'
+
+local fern_group = vim.api.nvim_create_augroup('ferngroup', {clear = true})
+vim.api.nvim_create_autocmd("FileType", {
+  group = fern_group,
+  pattern = "fern",
+  command = "setlocal nornu | setlocal nonu"
+})
+
 -- User commands
 vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
 
@@ -137,14 +153,14 @@ vim.api.nvim_create_autocmd(
   {'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter'},
   {
     group = number_toggle_group,
-    command = 'set rnu'
+    command = 'if &ft != "fern" | set rnu | endif'
   }
 )
 vim.api.nvim_create_autocmd(
   {'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave'},
   {
     group = number_toggle_group,
-    command = 'set nornu'
+    command = 'if &ft != "fern" | set nornu | endif'
   }
 )
 
