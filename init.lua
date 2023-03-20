c7f0f64142f092b28ed2ba77a9363ef647ed8bc6
Lucas Limeira <lucasalveslm@gmail.com>
2023-03-20 16:16:58 -0300

    moved keymaps and options to modules

--- init.lua
+++ init.lua
@@ -1,24 +1,5 @@
--- Options
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.hlsearch = false
-vim.opt.wrap = false
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.showmode = false
-vim.opt.updatetime = 750
-
--- Keybindings
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set({'n', 'x'}, 'cp', '"+y')
-vim.keymap.set({'n', 'x'}, 'cv', '"+p')
-vim.keymap.set({'n', 'x'}, 'x', '"_x')
-vim.keymap.set('x', '<Enter>', '<Esc>')
-
--- Leader
-vim.g.mapleader = ' '
+require('config')
+require('keymap')
 
 -- Lazy.nvim
 local lazy = {}
---
+++ lua/config.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.showmode = false
+vim.opt.updatetime = 750
+
+vim.g.mapleader = ' '
+
