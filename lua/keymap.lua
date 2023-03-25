7269d77650c7812914ed8bce7c73d4dd84a58895
Lucas Limeira <lucasalveslm@gmail.com>
2023-03-25 13:12:11 -0300

    keymaps to cycle through/close buffers

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,3 +3,7 @@ vim.keymap.set({'n', 'x'}, 'cp', '"+y')
 vim.keymap.set({'n', 'x'}, 'cv', '"+p')
 vim.keymap.set({'n', 'x'}, 'x', '"_x')
 vim.keymap.set('x', '<Enter>', '<Esc>')
+
+vim.keymap.set('n', '<Tab>', ':bnext<cr>')
+vim.keymap.set('n', '<S-Tab>', ':bprev<cr>')
+vim.keymap.set('n', '<leader>w', ':bd<cr>')
