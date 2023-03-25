616ea27e20bb7e866d2be8f2724f522fa376962b
Lucas Limeira <lucasalveslm@gmail.com>
2023-03-25 13:01:54 -0300

    keybindings for fern

--- lua/plugin/fern.lua
+++ lua/plugin/fern.lua
@@ -14,6 +14,18 @@ vim.api.nvim_create_autocmd('FileType', {
   group = fern_group,
   pattern = 'fern',
   callback = function()
+    vim.cmd(
+      'nmap <buffer><expr>'..
+      ' <Plug>(fern-my-open-expand-or-collapse)'..
+      ' fern#smart#leaf('..
+      ' "\\<Plug>(fern-action-open)",'..
+      ' "\\<Plug>(fern-action-expand)",'..
+      ' "\\<Plug>(fern-action-collapse)",'..
+      ')'
+  )
+
+    vim.keymap.set('n', '<2-LeftMouse>', '<Plug>(fern-my-open-expand-or-collapse)', {buffer = true, nowait = true})
+    vim.keymap.set('n', '<Enter>', '<Plug>(fern-my-open-expand-or-collapse)', {buffer = true, nowait = true})
     vim.keymap.set('n', 'o', '<Plug>(fern-action-open:edit)', {buffer = true})
     vim.keymap.set('n', 'v', '<Plug>(fern-action-open:vsplit)', {buffer = true})
     vim.keymap.set('n', 'n', '<Plug>(fern-action-new-path)', {buffer = true})
@@ -22,5 +34,9 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.keymap.set('n', 's', '<Plug>(fern-action-mark:toggle)', {buffer = true})
     vim.keymap.set('n', 'r', '<Plug>(fern-action-reload)', {buffer = true})
     vim.keymap.set('n', 'm', '<Plug>(fern-action-move)', {buffer = true})
+    vim.keymap.set('n', '<', '<Plug>(fern-action-leave)', {buffer = true, nowait = true})
+    vim.keymap.set('n', '>', '<Plug>(fern-action-enter)', {buffer = true, nowait = true})
+
+    vim.keymap.set('n', 'q', ':<C-u>quit<cr>', {buffer = true})
   end
 })
